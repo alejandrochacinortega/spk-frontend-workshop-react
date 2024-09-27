5b3db8c96759903ba31d113d197bb6655c25f567
alejandroortega <alejandro.chacin.ortega@gmail.com>
2024-09-27 16:15:42 +0200

    remove uneeded code

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import React, { useState } from 'react';
-import { Trip } from './types';
-
-import './App.css';
-import { data } from './data';
-import TripList from './components/trip.list/TripList';
-import AddTrip from './components/add-trip/AddTrip';
-import SearchTrip from './components/search-trip/SearchTrip';
+import React from 'react';
 
 const App: React.FC = () => {
-  const [trips, setTrips] = useState<Trip[]>(data);
-  const [searchDestination, setSearchDestination] = useState<string>('');
-
-  const addTrip = (trip: Trip) => {
-    setTrips([...trips, trip]);
-  };
-
-  const filteredTrips = trips.filter((trip) =>
-    searchDestination
-      ? trip.destination.toLowerCase().includes(searchDestination.toLowerCase())
-      : true,
-  );
-
   return (
     <div>
-      <h1>Travel Planner</h1>
-      <div className="trip-actions">
-        <AddTrip addTrip={addTrip} />
-        <SearchTrip
-          searchDestination={searchDestination}
-          setSearchDestination={setSearchDestination}
-        />
-      </div>
-      <TripList trips={filteredTrips} />
+      <h1>Hello world</h1>
     </div>
   );
 };
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Trip {
-  id: number;
-  studentName: string;
-  destination: string;
-  date: string;
-}
