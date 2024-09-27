41095dc626b8ab974358983cf20a9ce36bee0a1c
alejandroortega <alejandro.chacin.ortega@gmail.com>
2024-09-27 16:31:59 +0200

    demo ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Welcome from './components/welcome/Welcome';
+import { HelloWorld } from './components/hello-world/HelloWorld';
 
 const App: React.FC = () => {
   return (
     <div>
       <Welcome />
+      <HelloWorld />
     </div>
   );
 };
---
+++ src/components/hello-world/HelloWorld.tsx
@@ -0,0 +1,10 @@
+export const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World in React</h1>
+      <h2>
+        My name is Alejandro and I am ready to learn this awesome framework
+      </h2>
+    </div>
+  );
+};
