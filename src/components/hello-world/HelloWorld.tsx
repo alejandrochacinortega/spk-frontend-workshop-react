1b158dcbeef39487e5d1aae5f221f4ba866f94e0
alejandroortega <alejandro.chacin.ortega@gmail.com>
2024-09-28 08:38:52 +0200

    demo ready

--- src/components/hello-world/HelloWorld.tsx
+++ src/components/hello-world/HelloWorld.tsx
@@ -1,8 +1,10 @@
+import './helloWorld.css';
+
 export const HelloWorld = () => {
   return (
     <div>
-      <h1>Hello World in React</h1>
-      <h2>
+      <h1 className="header-color">Hello World in React</h1>
+      <h2 className="header-color">
         My name is Alejandro and I am ready to learn this awesome framework
       </h2>
     </div>
