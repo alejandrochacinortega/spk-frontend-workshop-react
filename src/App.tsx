2c9464272c98deaa5154a015b9e6c24ab3495a8d
Alejandro Ortega <alejandro.chacin.ortega@spk.no>
2024-10-01 12:02:42 +0200

    user done

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,30 @@ const App: React.FC = () => {
       <Welcome />
       <h2>Demo 5 </h2>
       <UsersList users={users} />
+      <UsersList
+        users={users}
+        eyeColor="brown"
+        title="People with brown eyes"
+      />
+      <UsersList users={users} minAge={39} title="People olden than 39" />
+      <UsersList
+        users={users}
+        favoriteFruit="apple"
+        title="People who have apple as their favorite fruit"
+      />
+      <UsersList
+        users={users}
+        favoriteFruit="apple"
+        minAge={30}
+        title="People who have apple as their favorite fruit and are older than 30"
+      />
+      <UsersList
+        users={users}
+        favoriteFruit="strawberry"
+        minAge={20}
+        maxAge={29}
+        title="People who have strawberry as their favorite fruit, are older than 20 and younger than 29"
+      />
     </div>
   );
 };
--- src/components/users-list/UsersList.tsx
+++ src/components/users-list/UsersList.tsx
@@ -1,20 +1,60 @@
-import { IUser } from '../../api/types';
+import { EyeColor, FavoriteFruit, Gender, IUser } from '../../api/types';
 import './usersList.css';
 
 interface IUsersListProps {
+  eyeColor?: EyeColor;
+  favoriteFruit?: FavoriteFruit;
+  gender?: Gender;
+  minAge?: number;
+  maxAge?: number;
+  title?: string;
   users: IUser[];
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const UsersList = ({ users }: IUsersListProps) => {
-  const renderAllUsers = () => {
-    return <h3>I should return all users :(</h3>;
+const UsersList = ({
+  eyeColor,
+  favoriteFruit,
+  gender,
+  minAge,
+  maxAge,
+  title = 'List of people',
+  users,
+}: IUsersListProps) => {
+  const renderUsers = () => {
+    let result = users;
+    if (eyeColor) {
+      result = result.filter((user) => user.eyeColor === eyeColor);
+    }
+
+    if (gender) {
+      result = result.filter((user) => user.gender === gender);
+    }
+
+    if (favoriteFruit) {
+      result = result.filter((user) => user.favoriteFruit === favoriteFruit);
+    }
+
+    if (minAge) {
+      result = result.filter((user) => user.age >= minAge);
+    }
+
+    if (maxAge) {
+      result = result.filter((user) => user.age <= maxAge);
+    }
+
+    return (
+      <ul className="list">
+        {result.map((user: IUser) => {
+          return <p key={user._id}>{user.name}</p>;
+        })}
+      </ul>
+    );
   };
 
   return (
     <div>
-      <h2>All users</h2>
-      {renderAllUsers()}
+      <h2>{title}</h2>
+      {renderUsers()}
     </div>
   );
 };
