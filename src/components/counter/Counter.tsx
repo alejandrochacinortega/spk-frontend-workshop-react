9fc1d799de8648793ed4961c280c778551cfaa65
alejandroortega <alejandro.chacin.ortega@gmail.com>
2024-10-06 17:55:51 +0200

    demo ready

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -11,7 +11,6 @@ const Counter: React.FC = () => {
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 };
