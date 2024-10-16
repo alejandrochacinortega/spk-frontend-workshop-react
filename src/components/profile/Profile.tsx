3a0ce17d72d98b83a60a8c63b49340b2dc814f39
Alejandro Ortega <alejandro.chacin.ortega@spk.no>
2024-10-16 12:47:20 +0200

    demo done

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,31 +1,6 @@
-import { useEffect, useState } from 'react';
-import { getProfile, IProfile } from '../../api/profile';
-
 const Profile = () => {
-  const [profile, setProfile] = useState<IProfile | null>(null);
-  useEffect(() => {
-    const fetchProfile = async () => {
-      const response = await getProfile();
-
-      if (response.ok) {
-        setProfile(response.profile);
-      }
-    };
-    fetchProfile();
-  }, []);
-
-  if (!profile) {
-    return <div>Loading profile...</div>;
-  }
   return (
-    <div>
-      <h2>Profile</h2>
-      <p>First Name: {profile.firstName}</p>
-      <p>Last Name: {profile.lastName}</p>
-      <p>Age: {profile.age}</p>
-      <p>Email: {profile.email}</p>
-      <p>Phone: {profile.phone}</p>
-    </div>
+    <div style={{ color: 'blue' }}>I should return the user's profile :( </div>
   );
 };
 
